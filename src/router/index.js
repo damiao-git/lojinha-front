6384de35b61b62a57fc1203e20deea763ddd109c
Damião <jose.faustino@hfa.mil.br>
2024-09-27 10:04:14 -0300

    conf and initial navbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import StudentsView from '@/views/Students/StudentsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/students',
+      name: 'students',
+      component: StudentsView
     },
     {
       path: '/about',
